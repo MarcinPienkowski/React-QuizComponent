2659288551d3b83a160de3e4109cfb3e4b1478a8
MarcinPienkowski <m.pienkowski@gmail.com>
2018-09-20 11:10:20 +0200

    Completed the module2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import QuizQuestionButton from './QuizQuestionButton.js'
 
-class QuizQuestion extends Component{
+class QuizQuestion extends Component {
+  handleClick(buttonText) {
 
-    render(){
-        return (<main>
-            <section>
-              <p>{this.props.quiz_question.instruction_text}</p>
-            </section>
-            <section className="buttons">
-              <ul>
-                <li>{this.props.quiz_question.answer_options[0]}
-                </li>
-              </ul>
-            </section>
-          </main>);
-    }
+  }
+
+  render() {
+    return (<main>
+      <section>
+        <p>{this.props.quiz_question.instruction_text}</p>
+      </section>
+      <section className="buttons">
+        <ul>
+          {
+            this.props.quiz_question.answer_options.map((answer_option, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />;
+            })
+          }
+        </ul>
+      </section>
+    </main>);
+  }
 };
 
 export default QuizQuestion;
